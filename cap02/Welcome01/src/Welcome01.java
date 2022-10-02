3ef3c1334a8e981a679fd91295d7fbcfd8cd6cbf
dapcjunior <danielpereiracamposjunior@gmail.com>
2022-10-01 22:50:43 -0300

    welcome01.java
    Criando o primeiro exercicio em Java com a IDE Intelij IDEA

---
+++ cap02/Welcome01/src/Welcome01.java
@@ -0,0 +1,9 @@
+public class Welcome01
+
+{
+    public static void main(String[] args)
+
+    {
+        System.out.println("Welcome to the Java language Program");
+    }   // fim metodo main
+}   // fim da class
