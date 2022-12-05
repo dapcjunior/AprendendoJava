ebf11422019aeb506ae66285abb2e049a2738a6e
Daniel Pereira <danielpereiracamposjunior@gmail.com>
2022-12-05 15:00:05 -0300

    projetoElevador
    Add o  segundo progeto exemplo do livro para calcular quantas pessoas cabem  em um elevador para que nao  se eceda o  peso  total

---
+++ java-para-leigos/InstaladorElevador/src/InstaladorElevador.java
@@ -0,0 +1,17 @@
+public class InstaladorElevador
+{
+    public static void main(String[] args)
+    {
+        int pesoDeUmaPessoa;
+        int limitePesoElevador;
+        int numeroPessoasElevador;
+
+        pesoDeUmaPessoa = 68;
+        limitePesoElevador = 630;
+        numeroPessoasElevador = limitePesoElevador / pesoDeUmaPessoa;
+
+        System.out.println("Cabem");
+        System.out.println(numeroPessoasElevador);
+        System.out.println("pessoas no  elevador");
+    }
+}
