23fdd976ff0dc80cdbd2f54eadf34f5895e6efcf
Daniel Pereira <danielpereiracamposjunior@gmail.com>
2022-12-05 14:46:34 -0300

    milionario
    Primeiro exemplo do livro java para leigos

---
+++ java-para-leigos/Milionario/src/Milionario.java
@@ -0,0 +1,13 @@
+public class Milionario
+{
+    public static void main(String[] args)
+    {
+        double valorNaConta;
+
+        valorNaConta = 50.22;
+        valorNaConta = valorNaConta + 1000000.00;
+
+        System.out.println("Voçê tem $: " + valorNaConta);
+        System.out.println( " sua conta.");
+    }
+}
